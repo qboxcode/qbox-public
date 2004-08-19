45e29976e089bcfc0bbcbedef3c496b4ecaae0ee
Francois Gygi <fgygi@ucdavis.edu>
2004-08-19 00:00:00 +0000

    Fixed bug #27
    git-svn-id: http://qboxcode.org/svn/qb/trunk@248 cba15fb0-1239-40c8-b417-11db7ca47a34

--- src/release.C
+++ src/release.C
@@ -3,10 +3,10 @@
 // release.C
 //
 ////////////////////////////////////////////////////////////////////////////////
-// $Id: release.C,v 1.6 2004-08-18 23:58:56 fgygi Exp $
+// $Id: release.C,v 1.7 2004-08-19 00:00:00 fgygi Exp $
 
 #include "release.h"
 std::string release(void)
 {
-  return std::string("1.19.0e");
+  return std::string("1.19.0");
 }

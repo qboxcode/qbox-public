6b1d3f63abe4ee2600f3ffaa163d175052da7589
Francois Gygi <fgygi@ucdavis.edu>
2004-10-28 16:57:33 +0000

    modified test to be relevant to wf->rho transforms.
    git-svn-id: http://qboxcode.org/svn/qb/trunk@286 cba15fb0-1239-40c8-b417-11db7ca47a34

--- src/testFourierTransform.C
+++ src/testFourierTransform.C
@@ -11,6 +11,11 @@ using namespace std;
 #include "FourierTransform.h"
 #include "Timer.h"
 
+int fft_flops(int n)
+{
+  return 5.0 * n * log((double) n) / log(2.0);
+}
+
 int main(int argc, char **argv)
 {
   Timer tm;
@@ -27,7 +32,7 @@ int main(int argc, char **argv)
 
   Context ctxt_global;
   int mype = ctxt_global.mype();
-  //cout << " Process " << mype << " on " << processor_name << endl;
+  cout << " Process " << mype << " on " << processor_name << endl;
 
   if ( argc != 5 )
   {
@@ -50,21 +55,30 @@ int main(int argc, char **argv)
   Basis basis(ctxt,kpoint);
   basis.resize(cell,cell,ecut);
   
-  FourierTransform ft(basis,basis.np(0),basis.np(1),basis.np(2));
-  
+  // transform and interpolate as for wavefunctions
+
+  FourierTransform ft2(basis,2*basis.np(0),2*basis.np(1),2*basis.np(2));
+  vector<complex<double> > f2(ft2.np012loc());
+
   vector<complex<double> > x(basis.localsize());
   vector<complex<double> > x1(basis.localsize());
   vector<complex<double> > x2(basis.localsize());
-  double flops = 5.0 * ft.np012() * 
-    ( log((double)basis.np(0)) + log((double)basis.np(1)) + 
-      log((double)basis.np(2)) ) / log(2.0);
+
+  double flops = 2*basis.nrod_loc() *      fft_flops(ft2.np2()) +
+                 ft2.np1()/2 * ft2.np2() * fft_flops(ft2.np0()) +
+                 ft2.np0()   * ft2.np2() * fft_flops(ft2.np1());
   if ( ctxt.onpe0() )
   {
     cout << " wfbasis.size() = " << basis.size() << endl;
     cout << " wfbasis.np() = " << basis.np(0) << " " << basis.np(1)
          << " " << basis.np(2) << endl;
-    // cout << " flop count: " << flops << endl;
+    cout << " flop count: " << flops << endl;
   }
+  cout << " wfbasis.nrod_loc(): " << basis.nrod_loc() << endl;
+  cout << " zvec.size: " 
+       << 2*basis.nrod_loc()*ft2.np2() * sizeof(complex<double>)
+       << endl;
+
   cout.setf(ios::fixed,ios::floatfield);
   cout.setf(ios::right,ios::adjustfield);
   cout << setprecision(6);
@@ -81,148 +95,6 @@ int main(int argc, char **argv)
     x2[i] = y;
     // x[i] = complex<double>(y,y);
   }
-#endif
-
-#if 0
-  x[0] = 1.0;
-#endif
-  
-#if 0
-  vector<complex<double> > f(ft.np012loc());
-  
-  tm.reset();
-  tm.start();
-  ft.backward(&x[0],&f[0]);
-  tm.stop();
-  cout << " " << basis.np(0) << " " << basis.np(1)
-       << " " << basis.np(2) << " ";
-  cout << " bwd time: " << tm.cpu() << " / " << tm.real() 
-       << "    " << 1.e-6*flops/tm.real() << " MFlops" << endl;
-#endif
-  
-#if 0
-  for ( int i = 0; i < basis.localsize(); i++ )
-    cout << setw(8) << basis.gx(i) << " " 
-         << setw(8) << basis.gx(i+basis.localsize()) << " " 
-         << setw(8) << basis.gx(i+2*basis.localsize())
-         << "     " << x[i] << endl;
-  for ( int i = 0; i < ft.np0(); i++ )
-    for ( int j = 0; j < ft.np1(); j++ )
-      for ( int k = 0; k < ft.np2_loc(); k++ )
-        cout << mype << ": "
-             << i << " " << j << " " << k+ft.np2_first() << " "
-             << f[ft.index(i,j,k)] << endl;
-#endif
-             
-#if 0
-  // integral in r space must be 1.0
-  double sum,tsum = 0.0;
-  for ( int i = 0; i < f.size(); i++ ) 
-    tsum += real(f[i]);
-  MPI_Allreduce(&tsum,&sum,1,MPI_DOUBLE,MPI_SUM,ctxt.comm());
-  
-  if ( ctxt.onpe0() )
-    cout << " sum: " << sum * omega / ft.np012() << endl;
-#endif
-  
-  // transform and interpolate as for wavefunctions
-  FourierTransform ft2(basis,2*basis.np(0),2*basis.np(1),2*basis.np(2));
-  vector<complex<double> > f2(ft2.np012loc());
-
-#if 0
-  // integral in r space must be 1.0
-  tsum = 0.0;
-  for ( int i = 0; i < f2.size(); i++ ) 
-    tsum += real(f2[i]);
-  MPI_Allreduce(&tsum,&sum,1,MPI_DOUBLE,MPI_SUM,ctxt.comm());
-  
-  cout << " " << 2*basis.np(0) << " " << 2*basis.np(1)
-       << " " << 2*basis.np(2) << " ";
-  cout << " bwd time: " << tm.cpu() << " / " << tm.real() 
-  << "    " << 1.e-6*flops/tm.real() << " MFlops" << endl;
-  cout << " sum on f2: " << sum * omega / ft2.np012() << endl;
-#endif
-  
-#if 0
-  //////////////////////////////////////////////////////////////////////////////
-  // Integration of a 2-norm normalized plane wave
-  //////////////////////////////////////////////////////////////////////////////
-  
-  for ( int i = 0; i < basis.localsize(); i++ )
-  {
-    x[i] = 0.0;
-  }
-  if ( ctxt.myproc() == 0 ) x[1] = 1.0/sqrt(2.0);
-  
-  ft.backward(&x[0],&f[0]);
-
-#if 0
-  for ( int i = 0; i < basis.localsize(); i++ )
-    cout << basis.kv(3*i) << " " << basis.kv(3*i+1) << " " << basis.kv(3*i+2)
-         << "     " << x[i] << endl;
-  for ( int i = 0; i < ft.np0(); i++ )
-    for ( int j = 0; j < ft.np1(); j++ )
-      for ( int k = 0; k < ft.np2_loc(); k++ )
-        cout << mype << ": "
-             << i << " " << j << " " << k+ft.np2_first() << " "
-             << f[ft.index(i,j,k)] << endl;
-#endif
-             
-  // integral of f^2 in r space must be 1.0
-  tsum = 0.0;
-  for ( int i = 0; i < f.size(); i++ ) 
-    tsum += norm(f[i]);
-  MPI_Allreduce(&tsum,&sum,1,MPI_DOUBLE,MPI_SUM,ctxt.comm());
-  
-  cout << " sum pw^2: " << sum / ft.np012() << endl;
-  
-  //////////////////////////////////////////////////////////////////////////////
-  // Integration of a 2-norm normalized gaussian
-  //////////////////////////////////////////////////////////////////////////////
-  for ( int i = 0; i < basis.localsize(); i++ )
-  {
-    double g2 = basis.g2(i);
-    x[i] = 1.0 / sqrt(omega) * pow(2.0*M_PI*rc*rc,0.75) * 
-           exp( -0.25 * g2 * rc*rc );
-  }
-  
-  ft2.backward(&x[0],&f2[0]);
-  
-//   for ( int i = 0; i < basis.localsize(); i++ )
-//     cout << basis.kv(3*i) << " " << basis.kv(3*i+1) << " " << basis.kv(3*i+2)
-//          << "     " << x[i] << endl;
-//   for ( int i = 0; i < ft2.np0(); i++ )
-//     for ( int j = 0; j < ft2.np1(); j++ )
-//       for ( int k = 0; k < ft2.np2_loc(); k++ )
-//         cout << mype << ": "
-//              << i << " " << j << " " << k+ft2.np2_first() << " "
-//              << f2[ft2.index(i,j,k)] << endl;
-             
-  // integral of gaussian^2 in r space must be 1.0
-  tsum = 0.0;
-  for ( int i = 0; i < f.size(); i++ ) 
-    tsum += norm(f[i]);
-  MPI_Allreduce(&tsum,&sum,1,MPI_DOUBLE,MPI_SUM,ctxt.comm());
-  
-  cout << " sum gaussian^2: " << sum / ft.np012() << endl;
-  
-#endif
-
-  //////////////////////////////////////////////////////////////////////////////
-  // Test forward transform
-  //////////////////////////////////////////////////////////////////////////////
-  // initialize
-
-#if 0
-  for ( int i = 0; i < ft2.np0(); i++ )
-    for ( int j = 0; j < ft2.np1(); j++ )
-      for ( int k = 0; k < ft2.np2_loc(); k++ )
-         f2[ft2.index(i,j,k)] = 
-           cos(k*2*M_PI/ft2.np2()) + sin(i*2*M_PI/ft2.np0()) ;
-  ft2.forward(&f2[0],&x[0]);
-  for ( int i = 0; i < basis.localsize(); i++ )
-    cout << basis.kv(3*i) << " " << basis.kv(3*i+1) << " " << basis.kv(3*i+2)
-         << "   x[G]= " << x[i] << endl;
 #endif
 
   tm.reset();
@@ -358,3 +230,146 @@ int main(int argc, char **argv)
 #endif
   return 0;
 }
+
+/////////////////////////
+// other tests on ft
+//
+#if 0
+  FourierTransform ft(basis,basis.np(0),basis.np(1),basis.np(2));
+#endif
+
+#if 0
+  x[0] = 1.0;
+#endif
+  
+#if 0
+  vector<complex<double> > f(ft.np012loc());
+  
+  tm.reset();
+  tm.start();
+  ft.backward(&x[0],&f[0]);
+  tm.stop();
+  cout << " " << basis.np(0) << " " << basis.np(1)
+       << " " << basis.np(2) << " ";
+  cout << " bwd time: " << tm.cpu() << " / " << tm.real() 
+       << "    " << 1.e-6*flops/tm.real() << " MFlops" << endl;
+
+  for ( int i = 0; i < basis.localsize(); i++ )
+    cout << setw(8) << basis.gx(i) << " " 
+         << setw(8) << basis.gx(i+basis.localsize()) << " " 
+         << setw(8) << basis.gx(i+2*basis.localsize())
+         << "     " << x[i] << endl;
+  for ( int i = 0; i < ft.np0(); i++ )
+    for ( int j = 0; j < ft.np1(); j++ )
+      for ( int k = 0; k < ft.np2_loc(); k++ )
+        cout << mype << ": "
+             << i << " " << j << " " << k+ft.np2_first() << " "
+             << f[ft.index(i,j,k)] << endl;
+#endif
+             
+#if 0
+  // integral in r space must be 1.0
+  double sum,tsum = 0.0;
+  for ( int i = 0; i < f.size(); i++ ) 
+    tsum += real(f[i]);
+  MPI_Allreduce(&tsum,&sum,1,MPI_DOUBLE,MPI_SUM,ctxt.comm());
+  
+  if ( ctxt.onpe0() )
+    cout << " sum: " << sum * omega / ft.np012() << endl;
+#endif
+  
+#if 0
+  // integral in r space must be 1.0
+  tsum = 0.0;
+  for ( int i = 0; i < f2.size(); i++ ) 
+    tsum += real(f2[i]);
+  MPI_Allreduce(&tsum,&sum,1,MPI_DOUBLE,MPI_SUM,ctxt.comm());
+  
+  cout << " " << 2*basis.np(0) << " " << 2*basis.np(1)
+       << " " << 2*basis.np(2) << " ";
+  cout << " bwd time: " << tm.cpu() << " / " << tm.real() 
+  << "    " << 1.e-6*flops/tm.real() << " MFlops" << endl;
+  cout << " sum on f2: " << sum * omega / ft2.np012() << endl;
+#endif
+  
+#if 0
+  //////////////////////////////////////////////////////////////////////////////
+  // Integration of a 2-norm normalized plane wave
+  //////////////////////////////////////////////////////////////////////////////
+  
+  for ( int i = 0; i < basis.localsize(); i++ )
+  {
+    x[i] = 0.0;
+  }
+  if ( ctxt.myproc() == 0 ) x[1] = 1.0/sqrt(2.0);
+  
+  ft.backward(&x[0],&f[0]);
+
+#if 0
+  for ( int i = 0; i < basis.localsize(); i++ )
+    cout << basis.kv(3*i) << " " << basis.kv(3*i+1) << " " << basis.kv(3*i+2)
+         << "     " << x[i] << endl;
+  for ( int i = 0; i < ft.np0(); i++ )
+    for ( int j = 0; j < ft.np1(); j++ )
+      for ( int k = 0; k < ft.np2_loc(); k++ )
+        cout << mype << ": "
+             << i << " " << j << " " << k+ft.np2_first() << " "
+             << f[ft.index(i,j,k)] << endl;
+#endif
+             
+  // integral of f^2 in r space must be 1.0
+  tsum = 0.0;
+  for ( int i = 0; i < f.size(); i++ ) 
+    tsum += norm(f[i]);
+  MPI_Allreduce(&tsum,&sum,1,MPI_DOUBLE,MPI_SUM,ctxt.comm());
+  
+  cout << " sum pw^2: " << sum / ft.np012() << endl;
+  
+  //////////////////////////////////////////////////////////////////////////////
+  // Integration of a 2-norm normalized gaussian
+  //////////////////////////////////////////////////////////////////////////////
+  for ( int i = 0; i < basis.localsize(); i++ )
+  {
+    double g2 = basis.g2(i);
+    x[i] = 1.0 / sqrt(omega) * pow(2.0*M_PI*rc*rc,0.75) * 
+           exp( -0.25 * g2 * rc*rc );
+  }
+  
+  ft2.backward(&x[0],&f2[0]);
+  
+//   for ( int i = 0; i < basis.localsize(); i++ )
+//     cout << basis.kv(3*i) << " " << basis.kv(3*i+1) << " " << basis.kv(3*i+2)
+//          << "     " << x[i] << endl;
+//   for ( int i = 0; i < ft2.np0(); i++ )
+//     for ( int j = 0; j < ft2.np1(); j++ )
+//       for ( int k = 0; k < ft2.np2_loc(); k++ )
+//         cout << mype << ": "
+//              << i << " " << j << " " << k+ft2.np2_first() << " "
+//              << f2[ft2.index(i,j,k)] << endl;
+             
+  // integral of gaussian^2 in r space must be 1.0
+  tsum = 0.0;
+  for ( int i = 0; i < f.size(); i++ ) 
+    tsum += norm(f[i]);
+  MPI_Allreduce(&tsum,&sum,1,MPI_DOUBLE,MPI_SUM,ctxt.comm());
+  
+  cout << " sum gaussian^2: " << sum / ft.np012() << endl;
+  
+#endif
+
+  //////////////////////////////////////////////////////////////////////////////
+  // Test forward transform
+  //////////////////////////////////////////////////////////////////////////////
+  // initialize
+
+#if 0
+  for ( int i = 0; i < ft2.np0(); i++ )
+    for ( int j = 0; j < ft2.np1(); j++ )
+      for ( int k = 0; k < ft2.np2_loc(); k++ )
+         f2[ft2.index(i,j,k)] = 
+           cos(k*2*M_PI/ft2.np2()) + sin(i*2*M_PI/ft2.np0()) ;
+  ft2.forward(&f2[0],&x[0]);
+  for ( int i = 0; i < basis.localsize(); i++ )
+    cout << basis.kv(3*i) << " " << basis.kv(3*i+1) << " " << basis.kv(3*i+2)
+         << "   x[G]= " << x[i] << endl;
+#endif

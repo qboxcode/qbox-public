19435b03248279fca5398042e8f0ede6b55c224c
Francois Gygi <fgygi@ucdavis.edu>
2005-03-17 17:28:34 +0000

    added IA32 macro, various other updates
    git-svn-id: http://qboxcode.org/svn/qb/trunk@368 cba15fb0-1239-40c8-b417-11db7ca47a34

--- src/mcr.mk
+++ src/mcr.mk
@@ -3,25 +3,24 @@
 #  mcr.mk
 #
 #-------------------------------------------------------------------------------
-# $Id: mcr.mk,v 1.9 2004-09-14 22:24:11 fgygi Exp $
+# $Id: mcr.mk,v 1.10 2005-03-17 17:28:34 fgygi Exp $
 #
  PLT=LINUX
 #-------------------------------------------------------------------------------
- GCCDIR=/usr/lib/gcc-lib/i386-redhat-linux/3.2.3
+ GCCDIR=/usr/local/tools/gnu/gcc/3.3.3_chaos_2_ia32/lib
  MPIDIR=/usr/lib/mpi
- XERCESCDIR=$(HOME)/software/xml/xerces-c-src2_2_0
-#XERCESCDIR=$(HOME)/software/xml/xerces-c-src_2_5_0
+ XERCESCDIR=$(HOME)/software/xml/ia32/xerces-c-src2_2_0
  XERCESCLIBDIR=/usr/apps/qbox/lib
  PLTOBJECTS = readTSC.o
 
  CXX=icc
  LD=$(CXX)
 
- PLTFLAGS += -DUSE_FFTW -DUSE_CSTDIO_LFS -D_LARGEFILE_SOURCE \
+ PLTFLAGS += -DIA32 -DUSE_FFTW -DUSE_CSTDIO_LFS -D_LARGEFILE_SOURCE \
              -D_FILE_OFFSET_BITS=64 -DUSE_MPI -DSCALAPACK -DADD_ \
              -DAPP_NO_THREADS -DXML_USE_NO_THREADS -DUSE_XERCES
 
- FFTWDIR=$(HOME)/fftw/linux-pc-icc/fftw-2.1.3/fftw
+ FFTWDIR=$(HOME)/software/fftw/ia32/fftw-2.1.3/fftw
  BLASDIR=/opt/intel/mkl/lib/32
  #PAPIDIR=/usr/local/tools/papi
  
@@ -32,7 +31,7 @@
 #CXXFLAGS= -g -D$(PLT) $(INCLUDE) $(PLTFLAGS) $(DFLAGS) 
  CXXFLAGS= -O3 -xW -Zp16 -D$(PLT) $(INCLUDE) $(PLTFLAGS) $(DFLAGS) 
 
- LIBPATH = -L$(GCCDIR)/lib -L$(FFTWDIR) -L/usr/X11R6/lib \
+ LIBPATH = -L$(GCCDIR)/lib -L$(FFTWDIR)/.libs -L/usr/X11R6/lib \
            -L$(MPIDIR)/lib -L$(BLASDIR) \
            -L$(XERCESCLIBDIR) 
   
@@ -42,11 +41,10 @@
  
  LDFLAGS = $(LIBPATH) $(LIBS) 
 
- PLAT=INTEL
+ PLAT=IA32
  # Blacs libraries
  BLACSDBGLVL   = 0
- BLACSdir      = $(HOME)/lib
- BLACSFINIT    = $(BLACSdir)/blacsF77init_MPI-$(PLAT)-$(BLACSDBGLVL).a
+ BLACSdir      = $(HOME)/software/blacs/ia32/BLACS/LIB
  BLACSCINIT    = $(BLACSdir)/blacsCinit_MPI-$(PLAT)-$(BLACSDBGLVL).a
  BLACSLIB      = $(BLACSdir)/blacs_MPI-$(PLAT)-$(BLACSDBGLVL).a
 
@@ -54,19 +52,14 @@
  FBLACSLIB     = $(BLACSFINIT) $(BLACSLIB) $(BLACSFINIT)
 
  # Scalapack libraries
- SCALAPACK_DIR = $(HOME)/lib
- PBLASLIB      = $(SCALAPACK_DIR)/pblas_$(PLAT).a
- SCALAPACKLIB  = $(SCALAPACK_DIR)/scalapack_$(PLAT).a
- TOOLSLIB      = $(SCALAPACK_DIR)/tools_$(PLAT).a
- REDISTLIB     = $(SCALAPACK_DIR)/redist_$(PLAT).a
+ SCALAPACK_DIR = $(HOME)/software/scalapack/ia32/SCALAPACK
+ SCALAPACKLIB  = $(SCALAPACK_DIR)/libscalapack.a
 
-#LAPACKLIB = -llapack
-#BLASLIB = -lblas
  LAPACKLIB = -lmkl_lapack
  BLASLIB = -lmkl
 
  # Parallel libraries
- PLIBS = $(SCALAPACKLIB) $(PBLASLIB) $(TOOLSLIB) $(REDISTLIB) $(CBLACSLIB)
+ PLIBS = $(SCALAPACKLIB) $(PBLASLIB) $(CBLACSLIB)
 
 #-------------------------------------------------------------------------------
 #-------------------------------------------------------------------------------

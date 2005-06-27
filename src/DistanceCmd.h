bf678ace4dee6d314af68c164311eb7abd9952b2
Francois Gygi <fgygi@ucdavis.edu>
2005-06-27 22:35:26 +0000

    New functions for AtomSet manipulation and measurement.
    git-svn-id: http://qboxcode.org/svn/qb/trunk@415 cba15fb0-1239-40c8-b417-11db7ca47a34

---
+++ src/DistanceCmd.h
@@ -0,0 +1,74 @@
+////////////////////////////////////////////////////////////////////////////////
+//
+// DistanceCmd.h:
+//
+////////////////////////////////////////////////////////////////////////////////
+// $Id: DistanceCmd.h,v 1.1 2005-06-27 22:35:26 fgygi Exp $
+
+#ifndef DISTANCECMD_H
+#define DISTANCECMD_H
+
+#include <iostream>
+#include "UserInterface.h"
+#include "Sample.h"
+#include <cstdlib>
+
+class DistanceCmd : public Cmd
+{
+  public:
+
+  Sample *s;
+
+  DistanceCmd(Sample *sample) : s(sample) {};
+
+  char *name(void) const { return "distance"; }
+  char *help_msg(void) const
+  {
+    return 
+    "\n distance\n\n"
+    " syntax: distance name1 name2\n\n"
+    "   The distance command prints the distance between two atoms.\n\n";
+  }
+
+  int action(int argc, char **argv)
+  {
+    if ( argc != 3 )
+    {
+      if ( ui->onpe0() )
+      {
+        cout << " use: distance name1 name2" << endl;
+      }
+      return 1;
+    }
+    
+    string name1(argv[1]);                
+    string name2(argv[2]);                
+    Atom* a1 = s->atoms.findAtom(name1);  
+    Atom* a2 = s->atoms.findAtom(name2);  
+    if ( a1 == 0 || a2 == 0 )             
+    {                                     
+      // either a1 or a2 was not found
+      if ( ui->onpe0() )
+      {
+        if ( a1 == 0 )
+          cout << " <!-- DistanceCmd: atom " << name1 << " not defined -->"
+               << endl;
+        if ( a2 == 0 )
+          cout << " <!-- DistanceCmd: atom " << name2 << " not defined -->"
+               << endl;
+      }
+      return 1;
+    }
+    
+    if ( ui->onpe0() )
+    {
+      const double d = length(a1->position()-a2->position());
+      cout.setf(ios::fixed,ios::floatfield);
+      cout << " <!-- distance " << name1 << "-" << name2 << ": "  
+           << setprecision(3)                                    
+           << d << " (a.u.) / " << 0.529177*d << " (Ang)" << endl;     
+    }
+    return 0;
+  }
+};
+#endif
---
+++ src/MoveCmd.h
@@ -0,0 +1,101 @@
+////////////////////////////////////////////////////////////////////////////////
+//
+// MoveCmd.h:
+//
+////////////////////////////////////////////////////////////////////////////////
+// $Id: MoveCmd.h,v 1.1 2005-06-27 22:35:26 fgygi Exp $
+
+#ifndef MOVECMD_H
+#define MOVECMD_H
+
+#include <string>
+#include <cstdlib>
+#include <iostream>
+using namespace std;
+
+#include "UserInterface.h"
+#include "Sample.h"
+
+class MoveCmd : public Cmd
+{
+  public:
+
+  Sample *s;
+
+  MoveCmd(Sample *sample) : s(sample) {};
+
+  char *name(void) const { return "move"; }
+  char *help_msg(void) const
+  {
+    return 
+    "\n move\n\n"
+    " syntax: move atom_name {to|by} x y z \n\n"
+    "   The move command displaces an atom to a new position.\n"
+    "   The new position is defined by absolute coordinates (to) or\n"
+    "   by a relative displacement (by).\n\n";
+  }
+
+  int action(int argc, char **argv)
+  {
+    // move must have 6 arguments including the command name
+    if ( argc != 6 )
+    {
+      if ( ui->onpe0() )
+        cout << " use: move atom_name {to|by} x y z " << endl;
+      return 1;
+    }
+  
+    const string atom_name = argv[1];
+    const string mode = argv[2];
+    const string xs = argv[3];
+    const string ys = argv[4];
+    const string zs = argv[5];
+
+    Atom* pa = s->atoms.findAtom(atom_name);
+    if ( !pa )
+    {
+      if ( ui->onpe0() )
+        cout << " <!-- MoveCmd: could not find atom " << atom_name << " -->"
+             << endl;
+      return 1;
+    }
+
+    D3vector pos = pa->position();
+
+    double x = pos.x;
+    double y = pos.y;
+    double z = pos.z;
+ 
+    if ( mode == "to" )
+    {
+      if ( xs != "*" )
+        x = atof(argv[3]);
+      if ( ys != "*" )
+        y = atof(argv[4]);
+      if ( zs != "*" )
+        z = atof(argv[5]);
+      pos = D3vector(x,y,z);      
+    }
+    else if ( mode == "by" )
+    {
+      x += atof(argv[3]);
+      y += atof(argv[4]);
+      z += atof(argv[5]);
+      pos = D3vector(x,y,z);      
+    }
+    else
+    {
+      if ( ui->onpe0() )
+        cout << " <!-- MoveCmd: unknown mode -->" << endl;
+      return 1;
+    }
+
+    pa->set_position(pos);
+    if ( ui->onpe0() )
+      cout << " <!-- MoveCmd: atom " << atom_name << " moved to "
+           << pos << " -->" << endl;
+    
+    return 0;
+  }
+};
+#endif

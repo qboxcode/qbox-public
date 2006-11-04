0b9383107abfe5243e3f79167be9ce9d236e198e
Francois Gygi <fgygi@ucdavis.edu>
2006-11-04 20:22:55 +0000

    rel1_30_1
    git-svn-id: http://qboxcode.org/svn/qb/trunk@477 cba15fb0-1239-40c8-b417-11db7ca47a34

--- src/release.C
+++ src/release.C
@@ -3,10 +3,10 @@
 // release.C
 //
 ////////////////////////////////////////////////////////////////////////////////
-// $Id: release.C,v 1.31 2006-08-22 15:23:28 fgygi Exp $
+// $Id: release.C,v 1.32 2006-11-04 20:21:37 fgygi Exp $
 
 #include "release.h"
 std::string release(void)
 {
-  return std::string("1.30.0");
+  return std::string("1.30.1");
 }

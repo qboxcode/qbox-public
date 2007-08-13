cc9031ccee6d96b66a1679d52e3686fba41b6820
Francois Gygi <fgygi@ucdavis.edu>
2007-08-13 21:23:51 +0000

    rewritten to avoid dependencies
    git-svn-id: http://qboxcode.org/svn/qb/trunk@494 cba15fb0-1239-40c8-b417-11db7ca47a34

--- src/spline.C
+++ src/spline.C
@@ -1,119 +1,157 @@
-/*******************************************************************************
- *
- * spline.c
- *
- ******************************************************************************/
-
+///////////////////////////////////////////////////////////////////////////////
+//
+// spline.C
+//
+///////////////////////////////////////////////////////////////////////////////
+// $Id: spline.C,v 1.2 2007-08-13 21:23:51 fgygi Exp $
 #include "spline.h"
-#include <assert.h>
+#include <cassert>
 
-void spline(double *x, double *y, int n, double yp1, double ypn, double *y2)
+void tridsolve(int n, double* d, double* e, double* f, double* x)
 {
+  // solve the tridiagonal system Ax=b
+  // d[i] = a(i,i)
+  // e[i] = a(i,i+1) (superdiagonal of A, e[n-1] not defined)
+  // f[i] = a(i,i-1) (subdiagonal of A, f[0] not defined)
+  // x[i] = right-hand side b as input
+  // x[i] = solution as output
 
-  int i,k;
-  double p,qn,sig,un,*u = new double[n];
-
-  if ( yp1 >= 1.e30 )
+  for ( int i = 1; i < n; i++ )
   {
+    f[i] /= d[i-1];
+    d[i] -= f[i]*e[i-1];
+  }
+
+  for ( int i = 1; i < n; i++ )
+    x[i] -= f[i]*x[i-1];
+
+  x[n-1] /= d[n-1];
+
+  for ( int i = n-2; i >= 0; i-- )
+    x[i] = (x[i]-e[i]*x[i+1])/d[i];
+}
+
+void spline(int n, double *x, double *y, double yp_left, double yp_right, 
+  int bcnat_left, int bcnat_right, double *y2)
+{
+  const double third = 1.0/3.0;
+  const double sixth = 1.0/6.0;
+  double *d = new double[n];
+  double *e = new double[n];
+  double *f = new double[n];
+  if ( bcnat_left == 0 )
+  {
+    // use derivative yp_left at x[0]
+    const double h = x[1]-x[0];
+    assert(h>0.0);
+    d[0] = third*h;
+    e[0] = sixth*h;
+    f[0] = 0.0;
+    y2[0] = (y[1]-y[0])/h - yp_left;
+  }
+  else
+  {
+    // use natural spline at x[0]
+    d[0] = 1.0;
+    e[0] = 0.0;
+    f[0] = 0.0;
     y2[0] = 0.0;
-    u[0] = 0.0;
+  }
+  if ( bcnat_right == 0 )
+  {
+    // use derivative yp_right at x[n-1]
+    const double h = x[n-1]-x[n-2];
+    assert(h>0.0);
+    d[n-1] = third*h;
+    e[n-1] = 0.0;
+    f[n-1] = sixth*h;
+    y2[n-1] = yp_right - (y[n-1]-y[n-2])/h;
   }
   else
   {
-    y2[0] = -0.5;
-    assert ( x[1] - x[0] > 0.0 );
-    u[0] = ( 3.0 / (x[1]-x[0]) ) * ( (y[1]-y[0]) / (x[1]-x[0]) - yp1 );
+    // use natural spline at x[n-1]
+    d[n-1] = 1.0;
+    e[n-1] = 0.0;
+    f[n-1] = 0.0;
+    y2[n-1] = 0.0;
   }
 
-  for ( i = 1; i < n-1; i++ )
+  // tridiagonal matrix
+  for ( int i = 1; i < n-1; i++ )
   {
-    assert ( x[i+1] > x[i] );
-    sig = ( x[i] - x[i-1] ) / ( x[i+1] - x[i-1] );
-    p = sig * y2[i-1] + 2.0;
-    y2[i] = ( sig - 1.0 ) / p;
-    u[i] = ( 6.0 * ( ( y[i+1] - y[i] ) / ( x[i+1] - x[i] ) -
-                     ( y[i] - y[i-1] ) / ( x[i] - x[i-1] ) ) /
-             ( x[i+1] - x[i-1] ) - sig * u[i-1] ) / p;
+    const double hp = x[i+1]-x[i];
+    const double hm = x[i]-x[i-1];
+    assert(hp>0.0);
+    assert(hm>0.0);
+    d[i] = third * (hp+hm);
+    e[i] = sixth * hp;
+    f[i] = sixth * hm;
+    y2[i] = (y[i+1]-y[i])/hp - (y[i]-y[i-1])/hm;
   }
 
-  if ( ypn >= 1.e30 )
-  {
-    qn = 0.0;
-    un = 0.0;
-  }
-  else
-  {
-    qn = 0.5;
-    un = ( 3.0 / (x[n-1]-x[n-2]) ) * 
-         ( ypn - (y[n-1]-y[n-2]) / (x[n-1]-x[n-2]) );
-  }
+  tridsolve(n,d,e,f,y2);
 
-  y2[n-1] = ( un - qn * u[n-2] ) / ( qn * y2[n-2] + 1.0 );
-
-  for ( k = n-2; k >= 0; k-- )
-  {
-    y2[k] = y2[k] * y2[k+1] + u[k];
-  }
-
-  delete [] u;
+  delete [] d;
+  delete [] e;
+  delete [] f;
 }
 
-void splint (double *xa, double *ya, double *y2a, int n, double x, double *y)
+void splint (int n, double *xa, double *ya, double *y2a, double x, double *y)
 {
-  int k,khi,klo;
+  int k;
   double a,b,h;
 
-  klo = 0;
-  khi = n-1;
+  int kl = 0;
+  int kh = n-1;
 
-  while ( khi - klo > 1 )
+  while ( kh - kl > 1 )
   {
-    k = ( khi + klo ) / 2;
+    k = ( kh + kl ) / 2;
     if ( xa[k] > x )
-      khi = k;
+      kh = k;
     else
-      klo = k;
+      kl = k;
   }
 
-  h = xa[khi] - xa[klo];
+  h = xa[kh] - xa[kl];
   assert ( h > 0.0 );
 
-  a = ( xa[khi] - x ) / h;
-  b = ( x - xa[klo] ) / h;
+  a = ( xa[kh] - x ) / h;
+  b = ( x - xa[kl] ) / h;
 
-  *y = a * ya[klo] + b * ya[khi] + h * h * (1.0/6.0) *
-       ( (a*a*a-a) * y2a[klo] + (b*b*b-b) * y2a[khi] );
+  *y = a * ya[kl] + b * ya[kh] + h * h * (1.0/6.0) *
+       ( (a*a*a-a) * y2a[kl] + (b*b*b-b) * y2a[kh] );
 
 }
 
-void splintd (double *xa, double *ya, double *y2a,
- int n, double x, double *y, double *dy)
+void splintd (int n, double *xa, double *ya, double *y2a,
+              double x, double *y, double *dy)
 {
-  int k,khi,klo;
+  int k;
   double a,b,h;
 
-  klo = 0;
-  khi = n-1;
+  int kl = 0;
+  int kh = n-1;
 
-  while ( khi - klo > 1 )
+  while ( kh - kl > 1 )
   {
-    k = ( khi + klo ) / 2;
+    k = ( kh + kl ) / 2;
     if ( xa[k] > x )
-      khi = k;
+      kh = k;
     else
-      klo = k;
+      kl = k;
   }
 
-  h = xa[khi] - xa[klo];
+  h = xa[kh] - xa[kl];
   assert ( h > 0.0 );
 
-  a = ( xa[khi] - x ) / h;
-  b = ( x - xa[klo] ) / h;
+  a = ( xa[kh] - x ) / h;
+  b = ( x - xa[kl] ) / h;
 
-  *y = a * ya[klo] + b * ya[khi] + h * h * (1.0/6.0) *
-       ( (a*a*a-a) * y2a[klo] + (b*b*b-b) * y2a[khi] );
+  *y = a * ya[kl] + b * ya[kh] + h * h * (1.0/6.0) *
+       ( (a*a*a-a) * y2a[kl] + (b*b*b-b) * y2a[kh] );
 
-  *dy = ( ya[khi] - ya[klo] ) / h +
-        h * ( ( (1.0/6.0) - 0.5 * a * a ) * y2a[klo] +
-              ( 0.5 * b * b - (1.0/6.0) ) * y2a[khi] );
+  *dy = ( ya[kh] - ya[kl] ) / h +
+        h * ( ( (1.0/6.0) - 0.5 * a * a ) * y2a[kl] +
+              ( 0.5 * b * b - (1.0/6.0) ) * y2a[kh] );
 }
--- src/spline.h
+++ src/spline.h
@@ -1,13 +1,11 @@
-/*******************************************************************************
- *
- * spline.h
- *
- ******************************************************************************/
-
-#define SPLINE_FLAT_BC 0.0       /* Flat boundary condition (y'=0) */
-#define SPLINE_NATURAL_BC 1.e31  /* Natural boundary condition (Y"=0) */
-
-void spline(double *x, double *y, int n, double yp1, double ypn, double *y2);
-void splint (double *xa, double *ya, double *y2a, int n, double x, double *y);
-void splintd (double *xa, double *ya, double *y2a, 
- int n, double x, double *y, double *dy);
+///////////////////////////////////////////////////////////////////////////////
+//
+// spline.h
+//
+///////////////////////////////////////////////////////////////////////////////
+// $Id: spline.h,v 1.2 2007-08-13 21:23:51 fgygi Exp $
+void spline(int n, double *x, double *y, double yp_left, double yp_right, 
+            int bcnat_left, int bcnat_right, double *y2);
+void splint (int n, double *xa, double *ya, double *y2a, double x, double *y);
+void splintd (int n, double *xa, double *ya, double *y2a, 
+              double x, double *y, double *dy);

d75be5477d692586ec878b6b424a1eaf8adea881
Francois Gygi <fgygi@ucdavis.edu>
2007-10-19 17:11:34 +0000

    initial revision
    git-svn-id: http://qboxcode.org/svn/qb/trunk@520 cba15fb0-1239-40c8-b417-11db7ca47a34

---
+++ src/KpointCmd.h
@@ -0,0 +1,108 @@
+////////////////////////////////////////////////////////////////////////////////
+//
+// KpointCmd.h:
+//
+////////////////////////////////////////////////////////////////////////////////
+// $Id: KpointCmd.h,v 1.1 2007-10-19 17:11:34 fgygi Exp $
+
+#ifndef KPOINTCMD_H
+#define KPOINTCMD_H
+
+#include "UserInterface.h"
+#include "D3vector.h"
+#include "Sample.h"
+
+class KpointCmd : public Cmd
+{
+  public:
+
+  Sample *s;
+
+  KpointCmd(Sample *sample) : s(sample) {};
+
+  char *name(void) const { return "kpoint"; }
+  char *help_msg(void) const
+  {
+    return
+    "\n kpoint\n\n"
+    " syntax:\n\n"
+    "   kpoint add kx ky kz weight\n"
+    "   kpoint delete kx ky kz \n"
+    "   kpoint list\n\n";
+  }
+
+  int action(int argc, char **argv)
+  {
+    const bool onpe0 = s->ctxt_.onpe0();
+    if ( argc < 2 )
+    {
+      if ( onpe0 )
+        cout << help_msg();
+      return 1;
+    }
+    string subcmd(argv[1]);
+    if ( subcmd == "add" )
+    {
+      if ( argc != 6 )
+      {
+        if ( onpe0 )
+          cout << help_msg();
+        return 1;
+      }
+      double kx = atof(argv[2]);
+      double ky = atof(argv[3]);
+      double kz = atof(argv[4]);
+      double w = atof(argv[5]);
+      s->wf.add_kpoint(D3vector(kx,ky,kz),w);
+    }
+    else if ( subcmd == "delete" )
+    {
+      if ( argc != 5 )
+      {
+        if ( onpe0 )
+          cout << help_msg();
+        return 1;
+      }
+      double kx = atof(argv[2]);
+      double ky = atof(argv[3]);
+      double kz = atof(argv[4]);
+      s->wf.del_kpoint(D3vector(kx,ky,kz));
+    }
+    else if ( subcmd == "list" )
+    {
+      if ( argc != 2 )
+      {
+        if ( onpe0 )
+          cout << help_msg();
+        return 1;
+      }
+      if ( onpe0 )
+      {
+        cout << " <-- kpoint list: reciprocal lattice coordinates" << endl;
+        for ( int ikp = 0; ikp < s->wf.nkp(); ikp++ )
+        {
+          cout << " "
+               << s->wf.kpoint(ikp) << "    " << s->wf.weight(ikp) << endl;
+        }
+        cout << "    kpoint list: cartesian coordinates" << endl;
+        UnitCell u = s->wf.cell();
+        for ( int ikp = 0; ikp < s->wf.nkp(); ikp++ )
+        {
+          D3vector kp = s->wf.kpoint(ikp);
+          cout << " "
+               << kp * u.b(0) << " " << kp * u.b(1) << " " << kp * u.b(2)
+               << "    " << s->wf.weight(ikp) << endl;
+        }
+        cout << " -->" << endl;
+      }
+    }
+    else
+    {
+      if ( onpe0 )
+        cout << help_msg();
+    }
+
+    return 0;
+  }
+};
+#endif

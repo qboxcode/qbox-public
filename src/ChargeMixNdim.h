602e7022c7d8f21f4892b32bc137104484437d78
Francois Gygi <fgygi@ucdavis.edu>
2009-03-08 01:09:58 +0000

    ChargeMixNdim: maximum dimension of subspace used in Anderson acceleration of charge mixing.
    git-svn-id: http://qboxcode.org/svn/qb/trunk@679 cba15fb0-1239-40c8-b417-11db7ca47a34

---
+++ src/ChargeMixNdim.h
@@ -0,0 +1,70 @@
+////////////////////////////////////////////////////////////////////////////////
+//
+// Copyright (c) 2008 The Regents of the University of California
+//
+// This file is part of Qbox
+//
+// Qbox is distributed under the terms of the GNU General Public License
+// as published by the Free Software Foundation, either version 2 of
+// the License, or (at your option) any later version.
+// See the file COPYING in the root directory of this distribution
+// or <http://www.gnu.org/licenses/>.
+//
+////////////////////////////////////////////////////////////////////////////////
+//
+// ChargeMixNdim.h
+//
+////////////////////////////////////////////////////////////////////////////////
+// $Id: ChargeMixNdim.h,v 1.1 2009-03-08 01:09:58 fgygi Exp $
+
+#ifndef CHARGEMIXNDIM_H
+#define CHARGEMIXNDIM_H
+
+#include<iostream>
+#include<iomanip>
+#include<sstream>
+#include<stdlib.h>
+
+#include "Sample.h"
+
+class ChargeMixNdim : public Var
+{
+  Sample *s;
+
+  public:
+
+  char *name ( void ) const { return "charge_mix_ndim"; };
+
+  int set ( int argc, char **argv )
+  {
+    if ( argc != 2 )
+    {
+      if ( ui->onpe0() )
+      cout << " charge_mix_ndim must be a non-negative integer" << endl;
+      return 1;
+    }
+
+    int v = atoi(argv[1]);
+    if ( v < 0 )
+    {
+      if ( ui->onpe0() )
+        cout << " charge_mix_ndim must be non-negative" << endl;
+      return 1;
+    }
+    s->ctrl.charge_mix_ndim = v;
+    return 0;
+  }
+
+  string print (void) const
+  {
+     ostringstream st;
+     st.setf(ios::left,ios::adjustfield);
+     st << setw(10) << name() << " = ";
+     st.setf(ios::right,ios::adjustfield);
+     st << setw(10) << s->ctrl.charge_mix_ndim;
+     return st.str();
+  }
+
+  ChargeMixNdim(Sample *sample) : s(sample) { s->ctrl.charge_mix_ndim = 3; };
+};
+#endif

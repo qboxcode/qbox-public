84a119b8e573b10f1791cab5247f2b871f39bd03
Francois Gygi <fgygi@ucdavis.edu>
2009-11-30 02:36:00 +0000

    Linux x86_64 gcc serial version
    git-svn-id: http://qboxcode.org/svn/qb/trunk@749 cba15fb0-1239-40c8-b417-11db7ca47a34

---
+++ src/x8664_gcc_serial.mk
@@ -0,0 +1,55 @@
+#-------------------------------------------------------------------------------
+#
+# Copyright (c) 2009 The Regents of the University of California
+#
+# This file is part of Qbox
+#
+# Qbox is distributed under the terms of the GNU General Public License
+# as published by the Free Software Foundation, either version 2 of
+# the License, or (at your option) any later version.
+# See the file COPYING in the root directory of this distribution
+# or <http://www.gnu.org/licenses/>.
+#
+#-------------------------------------------------------------------------------
+#
+#  x8664_gcc_serial.mk
+#
+#-------------------------------------------------------------------------------
+# $Id: x8664_gcc_serial.mk,v 1.1 2009-11-30 02:36:00 fgygi Exp $
+#
+ PLT=Linux_x8664
+#-------------------------------------------------------------------------------
+ XERCESCDIR=$(HOME)/software/xml/xerces-c-src_2_8_0
+ FFTWDIR=$(HOME)/software/fftw/Linux_x8664/fftw-2.1.3/fftw
+ BLASDIR=$(HOME)/software/atlas/ATLAS/Linux_P4E64SSE3/lib
+ LAPACKDIR=$(HOME)/software/lapack/LAPACK
+
+ PLTOBJECTS = readTSC.o
+
+ CXX=/usr/bin/g++
+ LD=$(CXX)
+
+ PLTFLAGS += -DIA32 -DUSE_FFTW -D_LARGEFILE_SOURCE \
+             -D_FILE_OFFSET_BITS=64 -DADD_ \
+             -DAPP_NO_THREADS -DXML_USE_NO_THREADS -DUSE_XERCES
+
+
+ INCLUDE = -I$(FFTWDIR) -I$(XERCESCDIR)/include
+
+ CXXFLAGS= -g -Wunused -D$(PLT) $(INCLUDE) $(PLTFLAGS) $(DFLAGS)
+
+ LIBPATH = -L$(GCCDIR)/lib -L$(FFTWDIR)/.libs -L/usr/X11R6/lib \
+           -L$(LAPACKDIR) -L$(BLASDIR) \
+           -L$(XERCESCDIR)/lib
+
+ LIBS =  $(PLIBS) -lfftw \
+         -llapack -lf77blas -latlas -lm \
+         -Xlinker -Bstatic \
+          -lc -lgfortran -static-libgcc -lxerces-c \
+         -Xlinker -Bdynamic
+
+ LDFLAGS = $(LIBPATH) $(LIBS)
+
+ PLAT=Linux_x8664
+
+#-------------------------------------------------------------------------------

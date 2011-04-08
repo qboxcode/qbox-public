f4fb16634cadced057390d8b26d345e2feabc62b
Francois Gygi <fgygi@ucdavis.edu>
2011-04-08 05:42:18 +0000

    add rseed command
    git-svn-id: http://qboxcode.org/svn/qb/trunk@903 cba15fb0-1239-40c8-b417-11db7ca47a34

--- src/qb.C
+++ src/qb.C
@@ -63,6 +63,7 @@ using namespace std;
 #include "RandomizeWfCmd.h"
 #include "ResetVcmCmd.h"
 #include "RescaleVCmd.h"
+#include "RseedCmd.h"
 #include "RunCmd.h"
 #include "SaveCmd.h"
 #include "SetCmd.h"
@@ -255,6 +256,7 @@ int main(int argc, char **argv, char **envp)
   ui.addCmd(new RandomizeWfCmd(s));
   ui.addCmd(new ResetVcmCmd(s));
   ui.addCmd(new RescaleVCmd(s));
+  ui.addCmd(new RseedCmd(s));
   ui.addCmd(new RunCmd(s));
   ui.addCmd(new SaveCmd(s));
   ui.addCmd(new SetCmd(s));

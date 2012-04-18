006ec0bf72c545ef1c1e4c48360b908e7ae2f7d5
Francois Gygi <fgygi@ucdavis.edu>
2012-04-18 19:27:09 +0000

    added OpenMP pragmas
    git-svn-id: http://qboxcode.org/svn/qb/trunk@1149 cba15fb0-1239-40c8-b417-11db7ca47a34

--- src/SlaterDet.C
+++ src/SlaterDet.C
@@ -165,6 +165,7 @@ void SlaterDet::resize(const UnitCell& cell, const UnitCell& refcell,
           // transform each state from old basis to grid to new basis
           for ( int n = 0; n < nstloc(); n++ )
           {
+            # pragma omp parallel for
             for ( int i = 0; i < tmpr.size(); i++ )
               tmpr[i] = 0.0;
             ft1.backward(ctmp.cvalptr(n*ctmp.mloc()),&tmpr[0]);
@@ -280,6 +281,7 @@ void SlaterDet::compute_density(FourierTransform& ft,
         const double* psi = (double*) &tmp[0];
         int ii = 0;
         //tm_rhosum.start();
+        # pragma omp parallel for
         for ( int i = 0; i < np012loc; i++ )
         {
           const double psi1 = psi[ii];
@@ -302,6 +304,7 @@ void SlaterDet::compute_density(FourierTransform& ft,
         ft.backward(c_.cvalptr(n*c_.mloc()),&tmp[0]);
         const double* psi = (double*) &tmp[0];
         int ii = 0;
+        # pragma omp parallel for
         for ( int i = 0; i < np012loc; i++ )
         {
           const double psi1 = psi[ii];
@@ -323,6 +326,7 @@ void SlaterDet::compute_density(FourierTransform& ft,
       if ( fac > 0.0 )
       {
         ft.backward(c_.cvalptr(n*c_.mloc()),&tmp[0]);
+        # pragma omp parallel for
         for ( int i = 0; i < np012loc; i++ )
           rho[i] += fac * norm(tmp[i]);
       }
@@ -358,6 +362,7 @@ void SlaterDet::rs_mul_add(FourierTransform& ft,
       ft.backward(c_.cvalptr(n*mloc),
                  c_.cvalptr((n+1)*mloc),&tmp[0]);
       int ii = 0;
+      # pragma omp parallel for
       for ( int i = 0; i < np012loc; i++ )
       {
         const double psi1 = p[ii];
@@ -378,6 +383,7 @@ void SlaterDet::rs_mul_add(FourierTransform& ft,
       const int n = nstloc()-1;
       ft.backward(c_.cvalptr(n*mloc),&tmp[0]);
       int ii = 0;
+      # pragma omp parallel for
       for ( int i = 0; i < np012loc; i++ )
       {
         const double psi1 = p[ii];
@@ -399,6 +405,7 @@ void SlaterDet::rs_mul_add(FourierTransform& ft,
     for ( int n = 0; n < nstloc(); n++ )
     {
       ft.backward(c_.cvalptr(n*mloc),&tmp[0]);
+      # pragma omp parallel for
       for ( int i = 0; i < np012loc; i++ )
         tmp[i] *= v[i];
       ft.forward(&tmp[0], &ctmp[0]);
@@ -526,6 +533,7 @@ void SlaterDet::riccati(const SlaterDet& sd)
       // x = s - 0.5 * ( r - xm )^T * ( r - xm )
       // Note: t and r are not symmetric, x, xm, and s are symmetric
 
+      # pragma omp parallel for
       for ( int i = 0; i < t.size(); i++ )
         t[i] = r[i] - xm[i];
 
@@ -535,6 +543,7 @@ void SlaterDet::riccati(const SlaterDet& sd)
       // get full matrix x
       x.symmetrize('l');
 
+      # pragma omp parallel for
       for ( int i = 0; i < t.size(); i++ )
         t[i] = x[i] - xm[i];
 
@@ -586,6 +595,7 @@ void SlaterDet::riccati(const SlaterDet& sd)
       // x = s - 0.5 * ( r - xm )^H * ( r - xm )
       // Note: t and r are not hermitian, x, xm, and s are hermitian
 
+      # pragma omp parallel for
       for ( int i = 0; i < t.size(); i++ )
         t[i] = r[i] - xm[i];
 
@@ -593,6 +603,7 @@ void SlaterDet::riccati(const SlaterDet& sd)
       x.herk('l','c',-0.5,t,1.0);
       x.symmetrize('l');
 
+      # pragma omp parallel for
       for ( int i = 0; i < t.size(); i++ )
         t[i] = x[i] - xm[i];
 
@@ -652,11 +663,13 @@ void SlaterDet::lowdin(void)
       // t now contains X^-T
 
       // xp = 0.5 * ( x + x^-T );
+      # pragma omp parallel for
       for ( int i = 0; i < x.size(); i++ )
         xp[i] = 0.5 * ( x[i] + t[i] );
 
 
       // Next lines: use t as temporary to compute || x - xp ||_F
+      # pragma omp parallel for
       for ( int i = 0; i < t.size(); i++ )
         t[i] = x[i] - xp[i];
 
@@ -799,11 +812,13 @@ void SlaterDet::ortho_align(const SlaterDet& sd)
       // t now contains X^-T
 
       // xp = 0.5 * ( x + x^-T );
+      # pragma omp parallel for
       for ( int i = 0; i < x.size(); i++ )
         xp[i] = 0.5 * ( x[i] + t[i] );
 
 
       // Next lines: use t as temporary to compute || x - xp ||_F
+      # pragma omp parallel for
       for ( int i = 0; i < t.size(); i++ )
         t[i] = x[i] - xp[i];
 
@@ -940,6 +955,7 @@ void SlaterDet::align(const SlaterDet& sd)
       // t now contains X^-T
 
       // xp = 0.5 * ( x + x^-T );
+      # pragma omp parallel for
       for ( int i = 0; i < x.size(); i++ )
         xp[i] = 0.5 * ( x[i] + t[i] );
 

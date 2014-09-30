45c7b31e53f60c6ee307df0b1a34f3eac75e4c6f
Francois Gygi <fgygi@ucdavis.edu>
2014-09-30 00:37:52 +0000

    added stepper kinetic energy to econst
    git-svn-id: http://qboxcode.org/svn/qb/trunk@1552 cba15fb0-1239-40c8-b417-11db7ca47a34

--- src/CPSampleStepper.C
+++ src/CPSampleStepper.C
@@ -244,8 +244,10 @@ void CPSampleStepper::step(int niter)
         cout << "  <temp_ion> " << mdionic_stepper->temp() << " </temp_ion>\n";
         cout << "  <eta_ion> " << mdionic_stepper->eta() << " </eta_ion>\n";
       }
-      cout << "  <econst> " << energy+ekin_ion+ekin_e << " </econst>\n";
-      cout << "  <ekin_ec> " << energy+ekin_ion+2*ekin_e << " </ekin_ec>\n";
+      double econst = energy + ekin_ion + ekin_e +
+                      mdionic_stepper->ekin_stepper();
+      cout << "  <econst> " << econst << " </econst>\n";
+      cout << "  <ekin_ec> " << econst + ekin_e << " </ekin_ec>\n";
     }
 
     if ( compute_stress )

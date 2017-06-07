f030ffc0450798b2e00ea1f3f9690ffdce840d69
Francois Gygi <fgygi@ucdavis.edu>
2017-06-07 16:30:36 -0700

    Fix file headers

--- src/uuid_str.C
+++ src/uuid_str.C
@@ -1,3 +1,21 @@
+////////////////////////////////////////////////////////////////////////////////
+//
+// Copyright (c) 2008 The Regents of the University of California
+//
+// This file is part of Qbox
+//
+// Qbox is distributed under the terms of the GNU General Public License
+// as published by the Free Software Foundation, either version 2 of
+// the License, or (at your option) any later version.
+// See the file COPYING in the root directory of this distribution
+// or <http://www.gnu.org/licenses/>.
+//
+////////////////////////////////////////////////////////////////////////////////
+//
+// uuid_str.C
+//
+////////////////////////////////////////////////////////////////////////////////
+
 #if USE_UUID
 #include <uuid/uuid.h>
 #include "uuid_str.h"

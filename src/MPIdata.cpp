06bad1dd34892f6c7dcf4183c8cf8505b9c7b363
Francois Gygi <fgygi@ucdavis.edu>
2020-07-06 15:47:49 -0700

    Reorder dimensions in MPI_Cart_comm
    Order {nkpb,nspb,nstb,ngb} makes global rank contiguous in
    the ngb direction.

--- src/MPIdata.cpp
+++ src/MPIdata.cpp
@@ -48,31 +48,33 @@ void MPIdata::set(int ngb, int nstb, int nspb, int nkpb)
   assert(nkpb>0);
 
   int ndims=4;
-  int dims[4] = { ngb, nstb, nspb, nkpb };
+  // Note: order of dimensions for Cart_comm to have contiguous tasks
+  // along the ngb dimension
+  int dims[4] = { nkpb, nspb, nstb, ngb };
   // plan for cyclic rotation of states blocks
-  int periods[4] = { 0, 1, 0, 0};
-  int reorder = 1;
+  int periods[4] = { 0, 0, 1, 0};
+  int reorder = 0;
 
   MPI_Cart_create(MPI_COMM_WORLD,ndims,dims,periods,reorder,&comm_);
 
   // create subcommunicators
   // G vector communicator
-  int g_remain_dims[4] = { 1, 0, 0, 0 };
+  int g_remain_dims[4] = { 0, 0, 0, 1 };
   MPI_Cart_sub(comm_,g_remain_dims,&g_comm_);
 
   // states communicator
-  int st_remain_dims[4] = { 0, 1, 0, 0 };
+  int st_remain_dims[4] = { 0, 0, 1, 0 };
   MPI_Cart_sub(comm_,st_remain_dims,&st_comm_);
 
   // spin communicator
-  int sp_remain_dims[4] = { 0, 0, 1, 0 };
+  int sp_remain_dims[4] = { 0, 1, 0, 0 };
   MPI_Cart_sub(comm_,sp_remain_dims,&sp_comm_);
 
   // kpoint communicator
-  int kp_remain_dims[4] = { 0, 0, 0, 1 };
+  int kp_remain_dims[4] = { 1, 0, 0, 0 };
   MPI_Cart_sub(comm_,kp_remain_dims,&kp_comm_);
 
   // Slater determinant communicator
-  int sd_remain_dims[4] = { 1, 1, 0, 0 };
+  int sd_remain_dims[4] = { 0, 0, 1, 1 };
   MPI_Cart_sub(comm_,sd_remain_dims,&sd_comm_);
 }

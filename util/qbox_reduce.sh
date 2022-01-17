a8525138787e1c64099b8608e1dd22eac74387a9
Francois Gygi <fgygi@ucdavis.edu>
2022-01-17 12:59:57 -0800

    Fix qbox_reduce.sh, remove dep on get_atomset.sh

--- util/qbox_reduce.sh
+++ util/qbox_reduce.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
-# reduce qbox restart file to atomset file
+# Reduce a qbox restart file to an atomset file
+# The original file is removed
 # use: qbox_reduce.sh file.xml [file.xml ..]
 for f in ${*}
 do
   name=${f%.xml}
   atomset_name=${name}_atomset
   echo $name.xml "->" $atomset_name.xml
-  get_atomset $f > $atomset_name.xml
+  nlines=$(grep /atomset -m 1 -n $f | cut -f1 -d: - )
+  head -$nlines $f > $atomset_name.xml
+  echo "</fpmd:sample>" >> $atomset_name.xml
   rm $f
 done

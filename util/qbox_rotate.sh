40c1c8bb6fdbd527d585e00b32c818598fef5bb9
Francois Gygi <fgygi@ucdavis.edu>
2023-07-24 11:55:48 -0700

    Add unit cell vector rotation in qbox_rotate.sh

--- util/qbox_rotate.sh
+++ util/qbox_rotate.sh
@@ -18,7 +18,16 @@ then
   awk -v c=$c -v s=$s \
   '/^#/ {print $0} \
   /species/ {print $0} \
-  /set cell/ {print $0} \
+  /set cell/ {ax=$3; ay=$4; az=$5; \
+              bx=$6; by=$7; bz=$8; \
+              cx=$9; cy=$10; cz=$11; \
+    printf("%s %12.6f %12.6f %12.6f  \
+                  %12.6f %12.6f %12.6f  \
+                  %12.6f %12.6f %12.6f\n", \
+            "set cell ", \
+            ax,c*ay-s*az,s*ay+c*az, \
+            bx,c*by-s*bz,s*by+c*bz, \
+            cx,c*cy-s*cz,s*cy+c*cz)} \
   /atom/ {x=$4; y=$5; z=$6; printf("%s %s %s %12.6f %12.6f %12.6f\n", \
   $1,$2,$3,x,c*y-s*z,s*y+c*z)}' $3
 elif [ $axis == 'y' ]
@@ -26,7 +35,16 @@ then
   awk -v c=$c -v s=$s \
   '/^#/ {print $0} \
   /species/ {print $0} \
-  /set cell/ {print $0} \
+  /set cell/ {ax=$3; ay=$4; az=$5; \
+              bx=$6; by=$7; bz=$8; \
+              cx=$9; cy=$10; cz=$11; \
+    printf("%s %12.6f %12.6f %12.6f  \
+                  %12.6f %12.6f %12.6f  \
+                  %12.6f %12.6f %12.6f\n", \
+            "set cell ", \
+            c*ax+s*az,ay,-s*ax+c*az, \
+            c*bx+s*bz,by,-s*bx+c*bz, \
+            c*cx+s*cz,cy,-s*cx+c*cz)} \
   /atom/ {x=$4; y=$5; z=$6; printf("%s %s %s %12.6f %12.6f %12.6f\n", \
   $1,$2,$3,c*x+s*z,y,-s*x+c*z)}' $3
 elif [ $axis == 'z' ]
@@ -34,7 +52,16 @@ then
   awk -v c=$c -v s=$s \
   '/^#/ {print $0} \
   /species/ {print $0} \
-  /set cell/ {print $0} \
+  /set cell/ {ax=$3; ay=$4; az=$5; \
+              bx=$6; by=$7; bz=$8; \
+              cx=$9; cy=$10; cz=$11; \
+    printf("%s %12.6f %12.6f %12.6f  \
+                  %12.6f %12.6f %12.6f  \
+                  %12.6f %12.6f %12.6f\n", \
+            "set cell ", \
+            c*ax-s*ay,s*ax+c*ay,az, \
+            c*bx-s*by,s*bx+c*by,bz, \
+            c*cx-s*cy,s*cx+c*cy,cz)} \
   /atom/ {x=$4; y=$5; z=$6; printf("%s %s %s %12.6f %12.6f %12.6f\n", \
   $1,$2,$3,c*x-s*y,s*x+c*y,z)}' $3
 else

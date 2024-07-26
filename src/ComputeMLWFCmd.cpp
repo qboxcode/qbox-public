193543ae8b32f5ae66c3f7d27daccdc07cbeb766
Francois Gygi <fgygi@ucdavis.edu>
2024-07-26 14:36:19 -0700

    Unify MLWF and dipole print format

--- src/ComputeMLWFCmd.cpp
+++ src/ComputeMLWFCmd.cpp
@@ -35,6 +35,10 @@ int ComputeMLWFCmd::action(int argc, char **argv)
   if ( wf.nkp()>1 || length(wf.kpoint(0)) != 0.0 )
     throw runtime_error("ComputeMLWFCmd: can only be used with 1 k-point");
 
+  // Check if nempty > 0
+  if ( wf.nempty() > 0 )
+    throw runtime_error("ComputeMLWFCmd: cannot run with nempty > 0");
+
   if ( onpe0 )
     cout << "<mlwfs>" << endl;
 
@@ -84,13 +88,25 @@ int ComputeMLWFCmd::action(int argc, char **argv)
   if ( onpe0 )
   {
     D3vector idipole = s->atoms.dipole();
-    cout << setprecision(6);
-    cout << "   <ionic_dipole> " << idipole
-         << " </ionic_dipole>" << endl;
-    cout << "   <total_dipole> " << idipole + edipole_sum
-         << " </total_dipole>" << endl;
-    cout << "   <total_dipole_length> " << length(idipole + edipole_sum)
-         << " </total_dipole_length>" << endl;
+    cout << "<dipole>" << endl;
+    cout << setprecision(8);
+    cout << " <dipole_ion>   "
+         << setw(14) << idipole.x
+         << setw(14) << idipole.y
+         << setw(14) << idipole.z
+         << " </dipole_ion>" << endl;
+    cout << " <dipole_el>    "
+         << setw(14) << edipole_sum.x
+         << setw(14) << edipole_sum.y
+         << setw(14) << edipole_sum.z
+         << " </dipole_el>" << endl;
+    D3vector dipole_total = idipole + edipole_sum;
+    cout << " <dipole_total> "
+         << setw(14) << dipole_total.x
+         << setw(14) << dipole_total.y
+         << setw(14) << dipole_total.z
+         << " </dipole_total>" << endl;
+    cout << "</dipole>" << endl;
   }
   return 0;
 }

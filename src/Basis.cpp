af0543628632a6efffcf5a95a83f2f7ce8fc8289
Francois Gygi <fgygi@ucdavis.edu>
2026-04-18 20:08:24 -0700

    Fix bug in Basis::factorizable

--- src/Basis.cpp
+++ src/Basis.cpp
@@ -122,7 +122,7 @@ bool Basis::factorizable(int n) const
   while ( ( n % 2 == 0 ) ) n /= 2;
   return ( n == 1 );
 #else
-  while ( n % 11 == 0 ) n /= 7;
+  while ( n % 11 == 0 ) n /= 11;
   while ( n % 7 == 0 ) n /= 7;
   while ( n % 5 == 0 ) n /= 5;
   while ( n % 3 == 0 ) n /= 3;
